739bd0e59b29730451b4ed22bd08f144da5e26f7
Jay Brodeur <jason.brodeur@gmail.com>
2018-05-10 16:48:53 -0400

    few updates

--- GCP_bulk_convert_working.m
+++ GCP_bulk_convert_working.m
@@ -1,10 +1,18 @@
-function [] = GCP_bulk_convert(process_dir, conv_flag)
+function [] = GCP_bulk_convert(process_dir, conv_flag, options)
 % process_dir: The directory (full path) within which all gcp files should be converted
 %%% *** NOTE: This function expects gcp files and tif files to be in the
 %%% same directory!! ***
 % conv_flag (required): specifies which type of conversion should be made
 %%%% conv_flag = 1: Convert from arcgis format to QGIS
 %%%% conv_flag = 2: Convert from QGIS to arcgis format
+
+if nargin < 3
+    options.ppi_in = [];
+    options.h = [];
+else
+    if isfield(options,'h')==0; options.h = [];end
+    if isfield(options,'ppi_in')==0; options.ppi_in = [];end
+end
 
 % Add a trailing slash on process_dir (if it doesn't already exist)
 if strcmp(process_dir(end),'\')==1 || strcmp(process_dir(end),'/')==1
@@ -23,18 +31,22 @@ switch conv_flag
                 %     d(ctr).name = tmp_dir(i).name; ctr = ctr+1;
                 %     fname2 = fname(1:strfind(fname,'.tif')-1);
                 % Check if there exists a a corresponding tif file
-                if exist([process_dir fname '.tif'],'file')==2; fname_tif = [fname '.tif'];
-                elseif exist([process_dir fname '.tiff'],'file')==2; fname_tif = [fname '.tiff'];
-                else
-                    fname_tif = '';
-                end
-                if ~isempty(fname_tif)
+                if isempty(options.ppi_in) % If options.ppi_in is empty, we need to extract the value from the tif file itself
+                    if exist([process_dir fname '.tif'],'file')==2; fname_tif = [fname '.tif'];
+                    elseif exist([process_dir fname '.tiff'],'file')==2; fname_tif = [fname '.tiff'];
+                    else
+                        fname_tif = '';
+                        continue;
+                    end
                     iminfo = imfinfo([process_dir fname_tif]); ppi_in = iminfo.XResolution; h = iminfo.Height;
-                    GCP_convert([process_dir tmp_dir(i).name],h, ppi_in,conv_flag)
+                else
+                    ppi_in = options.ppi_in;
+                    h = options.h;
                 end
+                GCP_convert([process_dir tmp_dir(i).name],h, ppi_in,conv_flag)
             end
         end
-    
+        
     case 2 % QGIS to ArcGIS
         for i = 1:1:length(tmp_dir)
             [fdir, fname, fext] = fileparts(tmp_dir(i).name); %file directory | filename | file extension
@@ -43,10 +55,20 @@ switch conv_flag
                 %     fname2 = fname(1:strfind(fname,'.tif')-1);
                 % Check if there exists a a corresponding tif file
                 fname_tif = fname;
-                if exist([process_dir fname_tif],'file')==2
-                    iminfo = imfinfo([process_dir fname_tif]); ppi_in = iminfo.XResolution; h = iminfo.Height;
-                    GCP_convert([process_dir tmp_dir(i).name],h, ppi_in,conv_flag)
+                if isempty(options.ppi_in)==1 || isempty(options.h)==1% If options.ppi_in or options.h is empty, we need to extract the value from the tif file itself
+                    if exist([process_dir fname_tif],'file')==2
+                        iminfo = imfinfo([process_dir fname_tif]); ppi_in = iminfo.XResolution; h = iminfo.Height;
+                    else
+                        continue;
+                    end
+                else
+                    ppi_in = options.ppi_in;
+                    h = options.h;
                 end
+                GCP_convert([process_dir tmp_dir(i).name],h, ppi_in,conv_flag)
             end
         end
 end
+
+
+
--- run_GCP_convert.m
+++ run_GCP_convert.m
@@ -6,3 +6,9 @@ GCP_bulk_convert(process_dir,conv_flag);
 process_dir = 'H:\Digitization_Projects\WWII_Topographic_Maps\Italy\Italy_100k_TIF_600dpi\North_Italy_Grid\';
 conv_flag = 2; % 1=ArcGIS to QGIS; 2=QGIS to ArcGIS
 GCP_bulk_convert(process_dir,conv_flag);
+
+%%
+process_dir = 'D:\Local\OCUL_HTDP\gcp\gcp-arc\1_25000\';
+conv_flag = 1; % 1=ArcGIS to QGIS; 2=QGIS to ArcGIS
+options.ppi_in = 600;
+GCP_bulk_convert(process_dir,conv_flag,options);
